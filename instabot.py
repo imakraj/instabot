fc4c25e7ce80a39ea12b36667242f4cf747dc846
Abhishek <akraj618@gmail.com>
2022-09-09 22:06:20 +0530

    Update instabot.py

--- instabot.py
+++ instabot.py
@@ -12,19 +12,22 @@ usernames = ["username1"]   #add usernames
 passwords = ["password"]  #add passwords
 accounts = dict(zip(usernames, passwords))
 
+#target account
+target_account = ""  #add target instagram account
+
 for username, password in accounts.items():
     #login
     my_bot.login(username=username, password=password)
 
     #like a post
     try:
-        my_bot.like_user("creative_kira", amount=1, filtration=False)
+        my_bot.like_user(target_account, amount=1, filtration=False)
     except Exception:
         print(Exception)
 
     #comment
     try:
-        user_id = my_bot.get_user_id_from_username("creative_kira")
+        user_id = my_bot.get_user_id_from_username(target_account)
         media_id = my_bot.get_last_user_medias(user_id, 1)
         my_bot.comment(media_id[0], "Hello, this is for test purpose only.") 
     except Exception:
